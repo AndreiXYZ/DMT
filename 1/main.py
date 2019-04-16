caba4dea96f6b4cdc8d8fc92c5a456611b2cbe4e
Andrei Apostol <apo.andrei23@yahoo.com>
2019-04-16 18:22:53 +0200

    almost done processing dataset

--- 1/main.py
+++ 1/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
+import pickle
 
 def convertToDatetime(dateString):
 	dateString = dateString.split(' ')[0]
@@ -19,7 +20,27 @@ sms_calls = df[ (df['variable']=='sms') | (df['variable']=='call') ].groupby(['v
 meaned_values = df[ (df['variable']!='sms') & (df['variable'] != 'call') ].groupby(['variable', 'time'], as_index=False).mean()
 
 # print(df.groupby(['variable', 'time'], as_index=False).mean())
-fulldf = pd.concat([sms_calls, meaned_values], keys='time')
-print(sms_calls.shape)
-print(meaned_values.shape)
-print(fulldf)
+fulldf = pd.concat([sms_calls, meaned_values], keys='time').reset_index().drop(['level_0', 'level_1', 'row'], axis=1)
+
+dates = fulldf['time'].unique()
+vals = fulldf['variable'].unique()
+
+tempdict = {key: [] for key in vals}
+tempdict['time'] = []
+
+for date in dates:
+	tempdict['time'].append(date)
+	for value in vals:
+		selection = fulldf[ (fulldf['time'] == date) & (fulldf['variable'] == value) ]
+		try:
+			tempdict[value].append(selection.iloc[0]['value'])
+		except Exception as e:
+			# TODO: Process mood, arousal and valence
+			tempdict[value].append(0)
+
+features_df = pd.DataFrame(tempdict)
+
+
+
+with open('processed_df.pkl', 'wb') as f:
+	pickle.dump(features_df, f)
