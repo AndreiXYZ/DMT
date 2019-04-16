05f0508a1535a7c278b78bae03688ae548aafd2b
Andrei Apostol <apo.andrei23@yahoo.com>
2019-04-16 17:04:32 +0200

    summed and meaned shit

--- 1/main.py
+++ 1/main.py
@@ -1,7 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime
+
+def convertToDatetime(dateString):
+	dateString = dateString.split(' ')[0]
+	return pd.to_datetime(dateString, format='%Y-%m-%d')
 
 df = pd.read_csv('dataset_mood_smartphone.csv')
 df.rename(columns={'Unnamed: 0' : 'row'}, inplace=True)
-# See how many entry points of each time we have
-print(df.groupby('variable').count()['row'])
+
+# Select just one patient for now
+df = df[ df['id'] == 'AS14.01']
+
+# Convert strings to datetime
+df['time'] = df['time'].apply(convertToDatetime)
+
+sms_calls = df[ (df['variable']=='sms') | (df['variable']=='call') ].groupby(['variable', 'time'], as_index=False).sum()
+meaned_values = df[ (df['variable']!='sms') & (df['variable'] != 'call') ].groupby(['variable', 'time'], as_index=False).mean()
+
+# print(df.groupby(['variable', 'time'], as_index=False).mean())
+fulldf = pd.concat([sms_calls, meaned_values], keys='time')
+print(sms_calls.shape)
+print(meaned_values.shape)
+print(fulldf)
