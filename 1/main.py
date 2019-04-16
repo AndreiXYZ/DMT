fc06f6a6adc139db9a4a826d52fca1db29eda0d8
Andrei Apostol <apo.andrei23@yahoo.com>
2019-04-16 19:12:23 +0200

    computed pkl files for all pacients separately, also for all pacients

--- 1/main.py
+++ 1/main.py
@@ -13,68 +13,71 @@ df = pd.read_csv('dataset_mood_smartphone.csv')
 df.rename(columns={'Unnamed: 0' : 'row'}, inplace=True)
 
 # Select just one patient for now
-df = df[ df['id'] == 'AS14.01']
-
-# Store the means for mood, arousal and valence
-means = {}
-means['mood'] = df[ df['variable']=='mood'].mean().iloc[1]
-means['arousal'] = df[ df['variable']=='circumplex.arousal'].mean().iloc[1]
-means['valence'] = df[ df['variable']=='circumplex.valence'].mean().iloc[1]
-
-# Convert strings to datetime
-df['time'] = df['time'].apply(convertToDatetime)
+ids = df['id'].unique()
 
 
-sms_calls = df[ (df['variable']=='sms') | (df['variable']=='call') ].groupby(['variable', 'time'], as_index=False).sum()
-meaned_values = df[ (df['variable']!='sms') & (df['variable'] != 'call') ].groupby(['variable', 'time'], as_index=False).mean()
+for id in ids:
+	df = df[ df['id'] == id]
+	# Store the means for mood, arousal and valence
+	means = {}
+	means['mood'] = df[ df['variable']=='mood'].mean().iloc[1]
+	means['arousal'] = df[ df['variable']=='circumplex.arousal'].mean().iloc[1]
+	means['valence'] = df[ df['variable']=='circumplex.valence'].mean().iloc[1]
+
+	# Convert strings to datetime
+	df['time'] = df['time'].apply(convertToDatetime)
 
 
-# print(df.groupby(['variable', 'time'], as_index=False).mean())
-fulldf = pd.concat([sms_calls, meaned_values], keys='time').reset_index().drop(['level_0', 'level_1', 'row'], axis=1)
+	sms_calls = df[ (df['variable']=='sms') | (df['variable']=='call') ].groupby(['variable', 'time'], as_index=False).sum()
+	meaned_values = df[ (df['variable']!='sms') & (df['variable'] != 'call') ].groupby(['variable', 'time'], as_index=False).mean()
 
 
-dates = sorted(fulldf['time'].unique())
-vals = fulldf['variable'].unique()
+	# print(df.groupby(['variable', 'time'], as_index=False).mean())
+	fulldf = pd.concat([sms_calls, meaned_values], keys='time').reset_index().drop(['level_0', 'level_1', 'row'], axis=1)
 
 
-tempdict = {key: [] for key in vals}
-tempdict['time'] = []
+	dates = sorted(fulldf['time'].unique())
+	vals = fulldf['variable'].unique()
 
 
-for date in dates:
-	tempdict['time'].append(date)
-	for value in vals:
-		selection = fulldf[ (fulldf['time'] == date) & (fulldf['variable'] == value) ]
-		try:
-			tempdict[value].append(selection.iloc[0]['value'])
-		except Exception as e:
-			if value=='mood':
-				if tempdict['mood']:
-					tempdict[value].append(tempdict['mood'][-1])
+	tempdict = {key: [] for key in vals}
+	tempdict['time'] = []
+
+
+	for date in dates:
+		tempdict['time'].append(date)
+		for value in vals:
+			selection = fulldf[ (fulldf['time'] == date) & (fulldf['variable'] == value) ]
+			try:
+				tempdict[value].append(selection.iloc[0]['value'])
+			except Exception as e:
+				if value=='mood':
+					if tempdict['mood']:
+						tempdict[value].append(tempdict['mood'][-1])
+					else:
+						tempdict[value].append(means['mood'])
+				elif value=='circumplex.arousal':
+					if tempdict['circumplex.arousal']:
+						tempdict[value].append(means['arousal'])
+					else:
+						tempdict[value].append(means['arousal'])
+				elif value=='circumplex.valence':
+					if tempdict['circumplex.valence']:
+						tempdict[value].append(means['valence'])
+					else:
+						tempdict[value].append(means['valence'])
 				else:
-					tempdict[value].append(means['mood'])
-			elif value=='circumplex.arousal':
-				if tempdict['circumplex.arousal']:
-					tempdict[value].append(means['arousal'])
-				else:
-					tempdict[value].append(means['arousal'])
-			elif value=='circumplex.valence':
-				if tempdict['circumplex.valence']:
-					tempdict[value].append(means['valence'])
-				else:
-					tempdict[value].append(means['valence'])
-			else:
-				tempdict[value].append(0)
+					tempdict[value].append(0)
 
-features_df = pd.DataFrame(tempdict)
+	features_df = pd.DataFrame(tempdict)
 
-print(features_df.head)
-corr_matrix = features_df.corr()
-sns.heatmap(corr_matrix, 
-			xticklabels=corr_matrix.columns.values,
-			yticklabels=corr_matrix.columns.values)
-plt.show()
-print(corr_matrix)
+	print(features_df.head)
+	# corr_matrix = features_df.corr()
+	# sns.heatmap(corr_matrix, 
+	# 			xticklabels=corr_matrix.columns.values,
+	# 			yticklabels=corr_matrix.columns.values)
+	# plt.show()
+	# print(corr_matrix)
 
-with open('processed_df.pkl', 'wb') as f:
-	pickle.dump(features_df, f)
+	with open('processed_df_' + id + '.pkl', 'wb') as f:
+		pickle.dump(features_df, f)
