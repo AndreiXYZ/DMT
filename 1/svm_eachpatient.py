f2d0876b2536aba1f0cd4423adf677152e845817
Orestis Kompougias <elaforestis@gmail.com>
2019-04-17 21:42:36 +0200

    next day pred plots

--- 1/svm_eachpatient.py
+++ 1/svm_eachpatient.py
@@ -5,9 +5,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 mseAllPatients=0
-runsPerPatient=100
+runsPerPatient=1
+#m=[]
 for file in os.listdir("datasets-per-pacient"):
 	if 'AS' in file:
 		df = pickle.load( open("datasets-per-pacient/"+file, "rb" ) )
@@ -22,18 +24,28 @@ for file in os.listdir("datasets-per-pacient"):
 		preprocessed=preprocessed.drop(preprocessed.columns[drop],axis=1)
 		X=preprocessed.drop(['mood'],axis=1)
 		y=preprocessed['mood']
+		X=X.to_numpy(copy=True)
+		y=y.to_numpy(copy=True)
+		daystopred=1
+		xtrain=X[0:-daystopred]
+		xtest=X[-daystopred:]
+		ytrain=y[0:-daystopred]
+		ytest=y[-daystopred:]
 		#totalmse=0
 		for i in range(runsPerPatient):
-			xtrain,xtest,ytrain,ytest=train_test_split(X,y,test_size=0.20)
-			classifier=SVR(kernel='rbf',gamma='auto')
+			#xtrain,xtest,ytrain,ytest=train_test_split(X,y,test_size=0.20)
+			classifier=SVR(kernel='rbf',degree=2,gamma='auto')
 			classifier.fit(xtrain,ytrain)
 			pred=classifier.predict(xtest)
-			#print('pred',pred)
-			actual=ytest.values
-			#print('actual',actual)
+			actual=ytest
 			mse=np.square(np.subtract(actual,pred)).mean()
-			#totalmse+=mse
 			mseAllPatients+=mse
-			#print('mse',mse)
-			#print(totalmse/1000)
+			plt.plot(np.append(ytrain,pred),'r')
+			plt.plot(y,'b')
+			plt.show()
+			#totalmse+=mse
+		#m.append(totalmse/runsPerPatient)
+		#print(totalmse/runsPerPatient,file)
 print(mseAllPatients/(27*runsPerPatient))
+#plt.hist(m)
+#plt.show()
