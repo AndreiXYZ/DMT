39f95a9077f61f7368c6256740a654aac2be8cd5
Orestis Kompougias <elaforestis@gmail.com>
2019-04-17 15:51:57 +0200

    SVM

---
+++ 1/svm.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import pickle
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVR
+import numpy as np
+
+df = pickle.load( open( "datasets-per-pacient/processed_df_AS14.01.pkl", "rb" ) )
+print(df)
+preprocessed=df.drop(['time','mood'],axis=1).rolling(6).mean().join(df['mood']).dropna()
+print(preprocessed)
+X=preprocessed.drop(['mood'],axis=1)
+y=preprocessed['mood']
+xtrain,xtest,ytrain,ytest=train_test_split(X,y,test_size=0.20)
+classifier=SVR(kernel='rbf')
+
+
+classifier.fit(xtrain,ytrain)
+pred=classifier.predict(xtest)
+print('pred',pred)
+actual=ytest.values
+print('actual',actual)
+print('mse',np.square(np.subtract(actual,pred)).mean())
