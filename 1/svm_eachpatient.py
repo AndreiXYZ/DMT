88632128e1e847d89a4ff4c451706597341f876b
Orestis Kompougias <elaforestis@gmail.com>
2019-04-17 18:01:00 +0200

    SVM for each patient

---
+++ 1/svm_eachpatient.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import pickle
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVR
+import numpy as np
+import os
+
+mseAllPatients=0
+runsPerPatient=100
+for file in os.listdir("datasets-per-pacient"):
+	if 'AS' in file:
+		df = pickle.load( open("datasets-per-pacient/"+file, "rb" ) )
+		#print(df)
+		preprocessed=df.drop(['time','mood'],axis=1).rolling(6).mean().join(df['mood']).dropna()
+		#print(preprocessed)
+		X=preprocessed.drop(['mood'],axis=1)
+		y=preprocessed['mood']
+		#totalmse=0
+		for i in range(runsPerPatient):
+			xtrain,xtest,ytrain,ytest=train_test_split(X,y,test_size=0.20)
+			classifier=SVR(kernel='rbf',gamma='auto')
+			classifier.fit(xtrain,ytrain)
+			pred=classifier.predict(xtest)
+			#print('pred',pred)
+			actual=ytest.values
+			#print('actual',actual)
+			mse=np.square(np.subtract(actual,pred)).mean()
+			#totalmse+=mse
+			mseAllPatients+=mse
+			#print('mse',mse)
+			#print(totalmse/1000)
+print(mseAllPatients/(27*runsPerPatient))
