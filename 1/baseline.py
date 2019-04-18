59956957f03b1db16ed95a85540c1b7071cae596
Traian <ntvidrascu@gmail.com>
2019-04-18 16:12:48 +0200

    plot baseline

--- 1/baseline.py
+++ 1/baseline.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pickle
 import os
+import matplotlib.pyplot as plt
 
 from pandas._libs.index import datetime, timedelta
 from sklearn import svm
@@ -29,7 +30,7 @@ def get_MSE(dataset):
             error += err
             times += 1
     error /= times
-    return np.sqrt(error)
+    return error
 
 
 errors = []
@@ -37,5 +38,26 @@ for filename in os.listdir("datasets-per-pacient"):
     error = get_MSE("datasets-per-pacient/" + filename)
     errors.append((error, filename))
 
+
+
+for i,error in enumerate(errors):
+    if i>0:
+        id = int(error[1].split('.')[1])
+    else:
+        id = 0
+    errors[i] = (error[0],id)
+
+
+
+y = []
+x = []
+
 for error in errors:
-    print(error)
+    x.append(error[1])
+    y.append(error[0])
+
+plt.plot(x,y, label='MSE')
+plt.grid()
+plt.legend()
+plt.title('MSE over all datasets')
+plt.show()
