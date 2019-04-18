286a23478e780b5306c95e0175c958d3b0d2f2d0
Traian <ntvidrascu@gmail.com>
2019-04-19 00:48:05 +0200

    statistics

--- 1/baseline.py
+++ 1/baseline.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
 import numpy as np
 
+
 def get_MSE(dataset):
     df = pickle.load(open(dataset, "rb"))
     predictions = []
@@ -24,14 +25,12 @@ def get_MSE(dataset):
     for prediction in predictions:
         date, mood = prediction
         if date in days.keys():
-            err = (days[date] - mood)**2
+            err = (days[date] - mood) ** 2
             errors.append(err)
             error += err
             times += 1
     error /= times
     return error
-
-
 
 
 errors = []
@@ -44,27 +43,46 @@ with open('mse.pkl', 'wb') as f:
 
 with open('mse.pkl', 'rb') as f:
     e = pickle.load(f)
-print(e)
 
-for i,error in enumerate(errors):
-    if i>0:
+for i, error in enumerate(errors):
+    if i > 0:
         id = int(error[1].split('.')[1])
     else:
         id = 0
-    errors[i] = (error[0],id)
-
-
+    errors[i] = (error[0], id)
 
 y = []
 x = []
 
-for error in errors:
+mean = 0.0
+maxi = 0.0
+mini = 1000.0
+for i,error in enumerate(errors):
     x.append(error[1])
     y.append(error[0])
+    if i >-1:
+        mean += y[-1]
+        if y[-1] > maxi:
+            maxi = y[-1]
+        if y[-1] < mini:
+            mini = y[-1]
 
-plt.plot(x,y, label='MSE')
+mean /= len(errors)
+
+variance = 0.0
+for i,error in enumerate(errors):
+    if i>-1:
+        variance+= (error[1] -mean)**2
+
+variance/= len(errors)
+std_dev = np.sqrt(variance)
+print('Avreage:' + str(mean))
+print('Variance:' + str(variance))
+print('Std dev:' + str(std_dev))
+print('Max:' + str(maxi))
+print('Min:' + str(mini))
+plt.plot(x, y, label='MSE')
 plt.grid()
 plt.legend()
 plt.title('MSE over all datasets')
 plt.show()
-
